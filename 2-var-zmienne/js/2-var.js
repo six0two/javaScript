2db0e145a61174f984245f4cda8e183b1d0ce183
six0two <eyescreem.022@gmail.com>
2018-02-10 16:14:22 +0100

    console log

---
+++ 2-var-zmienne/js/2-var.js
@@ -0,0 +1,10 @@
+var imie = "Marcin";
+var nazwisko = "Gałka";
+var wiek = 100;
+var wzrost;
+
+var wynik = "Imię:" + imie + " Nazwisko: " + nazwisko + " Wiek: " + wiek;
+console.log(imie);
+console.log(nazwisko);
+console.log(wiek);
+console.log(wynik);
