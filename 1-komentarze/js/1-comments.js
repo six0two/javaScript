10c6f769af79828501e3dcdf569f751eb65b8323
six0two <eyescreem.022@gmail.com>
2018-02-10 15:47:54 +0100

    komentarze

---
+++ 1-komentarze/js/1-comments.js
@@ -0,0 +1,11 @@
+/*
+    Komentarz blokowy
+    Dzięki niemu możęmy nasz komentarz umieścić w wilu liniach
+    
+    Skrót klawiaturowy CTRL + SHIFT + /
+
+*/
+
+//komentarz liniowy, skończy się razem z końcem tego wiersza, skrót CTRL + /
+
+
