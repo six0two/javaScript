435629254a73bc30eb01610afff86582cb87ad98
six0two <eyescreem.022@gmail.com>
2018-02-11 15:23:35 +0100

    switch

---
+++ 9-operatory_cd/instrukcje.js
@@ -0,0 +1,23 @@
+var grupa = 16;
+
+switch (grupa) {
+    case 10:
+        console.log("Liczba osób w grupie = 10");
+        break;
+        
+    case 12:
+        console.log("Liczba osób w grupie = 12");
+        break;
+        
+    case 14:
+        console.log("Liczba osób w grupie = 14");
+        break;
+        
+    case 16:
+        console.log("Liczba osób w grupie = 16");
+        break;
+        
+    default:
+        console.log("Nie wiadomo ile osób w grupie");
+        
+}
