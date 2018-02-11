6b821c619923eee6be767fb580428c523c453695
six0two <eyescreem.022@gmail.com>
2018-02-11 14:12:34 +0100

    operatory

---
+++ 8-operatory/operators.js
@@ -0,0 +1,26 @@
+var liczba1 = 14;
+var liczba2 = 6;
+
+console.log ( liczba1 * liczba2);
+//mnożenie
+
+var liczba1 = 14;
+var liczba2 = 6;
+
+console.log ( liczba1 / liczba2);
+//dzielnie
+
+var liczba1 = 14;
+var liczba2 = 6;
+
+console.log ( liczba1 % liczba2);
+//dzielnie modulo
+
+liczba1++;
+console.log(liczba1);
+//inkrementacja
+
+liczba1--;
+console.log(liczba1);
+//dekrementacja
+
